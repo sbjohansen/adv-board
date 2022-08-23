097760c83e0a935d06e5b4555e4f63a5ed998b6e
Slawomir Johansen <baran@fastmail.com>
2022-08-24 01:04:11 +0200

    TEST

--- client/src/index.js
+++ client/src/index.js
@@ -4,17 +4,14 @@ import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { BrowserRouter } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
-    </Provider>
-  </BrowserRouter>
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>
 );
