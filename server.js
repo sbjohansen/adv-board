6c18378752dc256505213c01fe95f80be4c6b944
Slawomir Johansen <baran@fastmail.com>
2022-08-23 22:48:29 +0200

    TEST heroku

--- server.js
+++ server.js
@@ -26,10 +26,7 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 //middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:8000"],
-  credentials: true 
-}));
+app.use(cors());
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
